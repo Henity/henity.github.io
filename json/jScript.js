a77b5e7c775782fd5e996cbc59078df37c74979f
Henity <henity17@gmail.com>
2017-11-02 16:12:10 -0400

    Update jScript.js

--- json/jScript.js
+++ json/jScript.js
@@ -2,7 +2,7 @@ var myRequest = new XMLHttpRequest();
 myRequest.open('GET', 'https://henity.github.io/json/people.json'); 
 myRequest.onload = function(){
 	console.log(myRequest.responseText);
-	var myData = myRequest.responceText;
+	var myData = myRequest.responseText;
 	console.log(myData[0]);
 };
 myRequest.send();
